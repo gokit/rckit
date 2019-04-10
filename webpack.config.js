3e74b375ccc81dd7cc7bdac1172c7adc29fd9f45
Alexander Ewetumo <trinoxf@gmail.com>
2019-04-10 13:51:52 +0800

    INIT: Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+const webpack = require("webpack");
+const path = require("path")
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+
+const rootDir = path.resolve(__dirname, ".");
+const srcDir = path.join(rootDir, "src");
+const distDir = path.join(rootDir, "dist");
+
+const htmlPlugin = new HtmlWebPackPlugin({
+  template: path.join(srcDir, 'index.html'),
+  filename: path.join(distDir, 'index.html'),
+});
+
+module.exports = {
+  entry: path.join(srcDir, './src/app.js'),
+  output: {
+    path: distDir,
+    publicPath: '/',
+    filename: 'app.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader']
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"]
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['*', '.js', '.jsx']
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+      htmlPlugin,
+  ],
+  devServer: {
+    contentBase: distDir,
+    hot: true,
+  }
+};
